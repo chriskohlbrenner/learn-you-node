3db94aada0c126219ebb1491fdd892167aca22f9
Chris Kohlbrenner <chrisk@redowlanalytics.com>
2014-10-20 07:58:21 -0400

    7-9 updated

--- 8/program.js
+++ 8/program.js
@@ -2,8 +2,12 @@ var http = require('http');
 
 http.get(process.argv[2], function(response){
   response.setEncoding('utf8')
+  allContent = ''
   response.on('data', function(data){
-    console.log(data)
+    allContent += data
   })
   response.on('error', console.log)
+  response.on('end', function(){
+    console.log(allContent.length + '\n' + allContent)
+  })
 })
