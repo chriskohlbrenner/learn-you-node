f9494492ebc8819e093f0277ced700d62ecb27e7
Chris Kohlbrenner <chrisk@redowlanalytics.com>
2014-10-25 18:25:00 -0400

    rm

--- app.js
+++
@@ -1,6 +0,0 @@
-http = require('http');
-http.createServer(function(request, response){
-  response.writeHead(200);
-  response.end("What's up?");
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/')
--- test.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.get(process.argv[2], function (response, reject) {
-  response.setEncoding('utf8')
-  response.on('data', function(data){
-    console.log(data);
-  });
-});
